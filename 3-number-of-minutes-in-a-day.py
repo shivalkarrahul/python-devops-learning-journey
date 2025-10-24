2729127d585f4b53e014c30ba137319560af7f1c
Rahul Shivalkar <rahulshivalkar@rediffmail.com>
2025-10-24 18:44:42 +0530

    number of secs in a day

--- 3-number-of-minutes-in-a-day.py
+++ 3-number-of-minutes-in-a-day.py
@@ -2,3 +2,4 @@ mins_in_one_hour = 60
 input_hour = 5
 print("Total mins in", input_hour ,"hours =", mins_in_one_hour*input_hour ,"mins")
 print("Total mins in a day =", mins_in_one_hour*24 ,"mins")
+print("Total sec in a day=", mins_in_one_hour*24*60, "sec")
