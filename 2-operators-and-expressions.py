a75f22d21cd46c5680c3ea9b3ceee532b08dac5c
Rahul Shivalkar <rahulshivalkar@rediffmail.com>
2025-10-24 18:36:42 +0530

    operators-and-expressions

---
+++ 2-operators-and-expressions.py
@@ -0,0 +1,6 @@
+number1 = 4
+number2= 4
+print("Multiplication of\t", number1 ,"and", number2 ,"=", number1*number2 )
+print("Addition of      \t", number1 ,"and", number2 ,"=", number1+number2 )
+print("Subtraction of   \t", number1 ,"and", number2 ,"=", number1-number2 )
+print("Division of      \t", number1 ,"and", number2 ,"=", number1/number2)
