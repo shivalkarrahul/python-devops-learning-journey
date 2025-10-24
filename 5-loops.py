46c8e6003dc35489af4dd85399d8644a30400d8e
Rahul Shivalkar <rahulshivalkar@rediffmail.com>
2025-10-24 21:28:10 +0530

    for loop

---
+++ 5-loops.py
@@ -0,0 +1,11 @@
+print("First for loop")
+for i in range(1, 11):
+    print(i)
+
+print("Second for loop")
+for j in range(1, 11, 2):
+    print(j)
+
+print("Third for loop")
+for k in range(11):
+    print(k)
