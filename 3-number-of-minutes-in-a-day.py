f119e063730700faea6548b57c8fab37da46636c
Rahul Shivalkar <rahulshivalkar@rediffmail.com>
2025-10-24 18:43:28 +0530

    number-of-minutes-in-a-day

---
+++ 3-number-of-minutes-in-a-day.py
@@ -0,0 +1,4 @@
+mins_in_one_hour = 60
+input_hour = 5
+print("Total mins in", input_hour ,"hours =", mins_in_one_hour*input_hour ,"mins")
+print("Total mins in a day =", mins_in_one_hour*24 ,"mins")
