6f6cbb047b51e73e21598033bae1644cc3009eb7
Rahul Shivalkar <rahulshivalkar@rediffmail.com>
2025-10-24 18:55:52 +0530

    build in functions

---
+++ 4-built-in-functions.py
@@ -0,0 +1,10 @@
+number1 = 5
+number2 = 2
+number3 = 9
+print("Absolute Value of", number1 ,"=", abs(number1))
+print("Absolute Value of", number2 ,"=", abs(number2))
+
+print("Power of", number1 ,"to", number2 ,"=", pow(number1, number2))
+
+print("Max in", number1 ,",", number2 ,"and", number3 ,"=", max(number1, number2, number3))
+print("Min in", number1, ",", number2, "and", number3, "=", min(number1, number2, number3))    
